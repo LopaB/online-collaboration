b8596df9afa6301cf6dcc0b02817aab14c2d4ba0
LopaB <lopamudra.bera@hotmail.com>
2017-03-12 18:32:13 +0530

    This is Hello World of Project2.

---
+++ Online-Collaborate/src/main/java/com/coll/OnlineCollaborateBackEnd/initializer/MvcWebApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.coll.OnlineCollaborateBackEnd.initializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.coll.OnlineCollaborateBackEnd.config.MvcConfig;
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {MvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
